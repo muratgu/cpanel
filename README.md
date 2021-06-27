4563f4de7a12256cbcefca8d516ffcad68510cc0
muratgu <mgungora@gmail.com>
2021-06-26 18:09:11 -0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# cpanel
+
+Commandline interface (CLI) to your cpanel account
+
+Create a file named `app.env` and add the following lines:
+```cmd
+   CPANEL_API_URL=https://myhosting.com:2083/execute
+   CPANEL_API_AUTH=cpanel myusername:myapikey
+   CPANEL_API_DOMAIN=mydomain.com
+```
+Run `cpanel -h` for usage information
+
+
+
---
+++ cmd/root.go
@@ -0,0 +1,124 @@
+/*
+Copyright © 2021 muratgu <mgungora@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"errors"
+	"strings"
+	"net/http"	
+	"net/url"
+	"encoding/json"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "cpanel",
+	Short: "Command line interface to North Shore Hikers",
+	Long: `
+cpanel is a CLI tool for North Shore Hikers account.
+`,
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./app.env)")
+
+	rootCmd.Version = "0.0.1"
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("app")
+		viper.SetConfigType("env")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Get(method string, values url.Values) (map[string]interface{}, error) {
+	url := viper.GetString("CPANEL_API_URL")
+	if url == "" {
+		log.Fatal("CPANEL_API_URL undefined")
+	}
+	auth := viper.GetString("CPANEL_API_AUTH")
+	if auth == "" {
+		log.Fatal("CPANEL_API_AUTH undefined")
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", url, method), strings.NewReader(values.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", auth)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode >= 200 && res.StatusCode <= 299 {
+        var data map[string]interface{}
+		json.Unmarshal(body, &data)
+		return data, nil
+	} else {
+		return nil, errors.New(fmt.Sprintf("HTTP Error %d", res.StatusCode))
+	}	
+}
+
+func IfSetElse(value bool, whenSet string, whenNotSet string) string {
+	if value {
+		return whenSet
+	} else {
+		return whenNotSet
+	}
+}
+
+func Println(data *string, err error) {
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(*data)
+	}
+}
